fc8b07525bfb41bb80678af226b9d05c64083b54
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2022-04-23 22:22:54 +0800

    Update demo.sh

--- src/demo.sh
+++ src/demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Train x2
 #python main.py --dir_data /media/data2/xiabin/datasets --n_GPUs 1 --rgb_range 1  --lr 1e-5 --decay 200-400-600-800 --epochs 1000 --chop --save_results --n_resblocks 32 --n_feats 256 --res_scale 0.1 --batch_size 32 --model NLSN --scale 4 --patch_size 96 --save NLSN_x4 --data_train DIV2K --data_test Set14 --test_every 500 --resume -1
-CUDA_VISIBLE_DEVICES=6 python main.py --dir_data /media/data2/xiabin/datasets --n_GPUs 1 --rgb_range 1  --save_models --lr 1e-4 --decay 200-400-600-800 --epochs 1000 --chop --save_results --n_resblocks 32 --n_feats 256 --res_scale 0.1 --batch_size 16 --model ENLCN --scale 4 --save ENLCN_x4 --data_train DIV2K --data_test Set14 --patch_size 96
+CUDA_VISIBLE_DEVICES=6 python main.py --dir_data /media/data2/xiabin/datasets --n_GPUs 1 --rgb_range 1  --save_models --lr 1e-4 --decay 200-400-600-800 --epochs 1000 --chop --save_results --n_resblocks 32 --n_feats 256 --res_scale 0.1 --batch_size 16 --model ENLCN --scale 4 --save ENLCN_x4 --data_train DIV2K --data_test Set14 --patch_size 192
 
 
 #Test x2
